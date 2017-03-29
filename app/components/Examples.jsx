67c5056e5bb4ac803b05e3f79cbbf7b7efe054db
= <=>
2017-03-28 19:50:49 -0600

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h2>Examples Component</h2>
+    <div>
+      <h3>Examples Component</h3>
+      <p>Welcome to example page!</p>
+    </div>
   )
 }
 module.exports = Examples;
